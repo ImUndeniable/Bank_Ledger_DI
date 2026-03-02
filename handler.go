d74a9d5fe019f0778f1c2bb1bc280ea7abfecc8f
ImUndeniable <tarun282000@gmail.com>
2026-03-02 19:02:28 +0530

    handler service

--- handler.go
+++ handler.go
@@ -1,9 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type TransferRequest struct {
-	From   string `json:"from"`
-	To     string `json:"to"`
-	Amount string `json:"amount"`
+	From   string  `json:"from"`
+	To     string  `json:"to"`
+	Amount float64 `json:"amount"`
 }
 
 // Dependency Injected - conn to service layer
@@ -17,4 +22,26 @@ func NewHandlerService(h TransferService) *HandlerService {
 	return &HandlerService{
 		service: h,
 	}
+}
+
+func (h *HandlerService) HandleTransfer(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusBadRequest)
+		return
+	}
+
+	var req TransferRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	err := h.service.SendMoney(req.From, req.To, req.Amount)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Transfer Successfull!\n"))
 }
